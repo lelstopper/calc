9746815ec5f965024137ea7e550d5a2c090b27a5
lolstopper <pranavagrawal929@gmail.com>
2021-01-27 20:26:21 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
 import tkinter as tk, tkinter.font as tkf
 import decimal
+from math import *
+
+#thanks to aditya agarwal for the lambda functon wizardry
 
 # ! TODO
 #debug related stuff
@@ -22,41 +25,44 @@ def buildExpression(char):
         if(not char.isdigit() and expression[-1] == '.'): return None
         if(char.isdigit() and expression[-1] == ')'): return None
         if(char == '(' and expression[-1].isdigit()): return None
+        
     expression += char
-    print(expression)
-
+    output.configure (text = expression)
+    
 def clearExpression():
     global expression
     expression = ""
-    print("All Cleared!")
+    output.config(text = 'All Cleared!')
 
 def delExpression():
     global expression
     expression = expression[:len(expression)-1]
     if(expression[-1] == '.'): expression = expression[:len(expression)-1]
-    print(expression)
+    output.config(text = expression)
 
 def execute():
     global expression
     try:
         expression = str(eval(expression))
     except:
-        print("Syntax Error!")
+        output.config(text = 'Syntax Error!')
         return None
+    
     expression = expression.split('.')
     if(len(expression) == 1 or int(expression[1])==0): expression = expression[0]
     else : expression = expression[0] + '.' + expression[1]
-    print(expression)
-
+    
+    output.configure (text = expression)
+    
 def getButton(char, callback):
     return tk.Button(root, width=wide, height=high, text=char, command=callback)
 
 #window
 root  = tk.Tk()
-root.geometry('400x400')
-root.title('calculator') 
+root.geometry('370x275')
+root.title('Calculator') 
 
-numberButtons = [(lambda num : tk.Button(root, text = num, command = lambda: buildExpression(num), height = high, width = wide ))(str(i)) for i in range(0, 10)]
+numberButtons = [(lambda num : tk.Button(root, text = num, command = lambda: buildExpression(num), height = high, width = wide, bg = '#aeaeae'))(str(i)) for i in range(0, 10)]
 
 # Simple arithmetic functions
 buttonEqual = getButton('=', execute)
@@ -67,18 +73,21 @@ buttonDiv = getButton('/', lambda : buildExpression('/'))
 buttonPow = getButton('^', lambda : buildExpression('**'))
 buttonDec = getButton('.', lambda : buildExpression('.'))
 buttonPerCent = getButton('%', lambda : buildExpression('*0.01'))
+buttonLBrack = getButton('(', lambda: buildExpression('(') )
+buttonRBrack = getButton(')', lambda: buildExpression(')') )
 
-#! TODO
 #log
+#radians->degrees
 #trig functions
-'''
-buttonSin = getButton('sin', lambda : sign('+'))
-buttonCos = getButton('cos', lambda : sign('+'))
-buttonTan = getButton('tan', lambda : sign('+'))
-buttonSec = getButton('sec', lambda : sign('+'))
-buttonCosec = getButton('csec', lambda : sign('+'))
-buttonCot = getButton('cot', lambda : sign('+'))
-'''
+
+buttonSin = getButton('sin', lambda : buildExpression('sin'))
+buttonCos = getButton('cos', lambda : buildExpression('cos'))
+buttonTan = getButton('tan', lambda : buildExpression('tan'))
+buttonSec = getButton('sec', lambda : buildExpression('1/cos'))
+buttonCosec = getButton('csc', lambda : buildExpression('1/sin'))
+buttonCot = getButton('cot', lambda : buildExpression('1/tan'))
+
+
 #bitwise Functions
 '''
 buttonNot = getButton('not', lambda : sign('+'))
@@ -87,21 +96,39 @@ buttonOr = getButton('or', lambda : sign('+'))
 buttonXor = getButton('xor', lambda : sign('+'))
 '''
 
+
 buttonDelete = getButton('CE', delExpression)
 buttonReset = getButton('AC', clearExpression)
 
 for i in range(1, 10): numberButtons[i].grid(row = int((i+2)//3), column = 1+((i-1)%3))
 numberButtons[0].grid(row = 4, column = 2)
 
-buttonEqual.grid(row = 4, column = 3)
 buttonPlus.grid(row = 1, column = 4)
-buttonMinus.grid(row = 2, column = 4)
-buttonProd.grid(row = 3, column = 4)
-buttonDiv.grid(row = 4, column = 4)
 buttonPow.grid(row = 1, column = 5)
-buttonDec.grid(row = 3, column = 5)
+
+buttonMinus.grid(row = 2, column = 4)
 buttonPerCent.grid(row = 2, column = 5)
+
+buttonProd.grid(row = 3, column = 4)
+buttonDelete.grid(row = 3, column = 5)
+
+buttonDec.grid(row = 4, column = 1)
+buttonEqual.grid(row = 4, column = 3)
+buttonDiv.grid(row = 4, column = 4)
 buttonReset.grid(row = 4, column = 5)
-buttonDelete.grid(row = 4, column = 1)
+buttonLBrack.grid(row = 4, column = 6)
+buttonRBrack.grid(row = 4, column = 7)
+
+
+buttonSin.grid(row = 1, column = 6)
+buttonCos.grid(row = 2, column = 6)
+buttonTan.grid(row = 3, column = 6)
+
+buttonSec.grid(row = 1, column = 7)
+buttonCosec.grid(row = 2, column = 7)
+buttonCot.grid(row = 3, column = 7)
+
+output = tk.Label(root, text='Enter your expression:' , bg = '#c9c9c9', width = 49, height = 3, relief = 'sunken', anchor = 'w', padx = 10, font=('Calibri', 10, 'bold'))
+output.grid(row = 5, column = 1, columnspan = 7, rowspan = 25)
 
 root.mainloop()
